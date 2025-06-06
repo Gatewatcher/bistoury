aca9cd0fffd10fe1c764871e54707507d33ba580
Benoit Chassignol <benoit.chassignol@gatewatcher.com>
2025-06-06 14:14:44 +0200

    feat(utils): add tests utils (#11)

---
+++ src/utils-tests/index.ts
@@ -0,0 +1,56 @@
+import {
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+
+import { ApiError, DefaultResponseError } from "./../utils-api";
+import { TestId, TestIdLoading } from "./../utils-types";
+
+export const suffixTestId = (baseId: TestId, suffix: TestId): TestId =>
+  `${baseId}-${suffix}`;
+
+export const expectToBeVisibleInTheDocument = async (
+  testId: TestId | RegExp,
+  method = screen.findByTestId
+) => {
+  const element = await method(testId);
+  expect(element).toBeInTheDocument();
+  expect(element).toBeVisible();
+};
+
+export const expectNotToBeVisibleInTheDocument = async (
+  testId: TestId | RegExp,
+  method = screen.queryByTestId
+) => {
+  const element = await method(testId);
+  expect(element).toBeNull();
+  expect(element).not.toBeInTheDocument();
+};
+
+export const expectIsLoading = async (
+  loaderId: TestIdLoading = "circular-loader"
+) => {
+  await expectToBeVisibleInTheDocument(loaderId);
+};
+
+export const expectDefaultResponseError = async (
+  error: ApiError | string = DefaultResponseError
+) => {
+  await waitFor(
+    async () => {
+      await expectToBeVisibleInTheDocument(
+        typeof error === "string" ? error : error.detail,
+        screen.findByText
+      );
+    },
+    { timeout: 3000 }
+  );
+};
+
+export const waitForLoaderToBeRemoved = async (
+  loaderId: TestIdLoading = "circular-loader"
+): Promise<any> => {
+  await waitForElementToBeRemoved(() => screen.getAllByTestId(loaderId));
+  await new Promise(process.nextTick);
+};
