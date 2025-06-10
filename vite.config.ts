4987505d8da5c5ead13d8d41db3492dfca538732
Benoit Chassignol <benoit.chassignol@gatewatcher.com>
2025-06-10 10:33:35 +0200

    feat(utils): add lint (#11)

---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: {
+        hooks: path.resolve(__dirname, 'src/hooks/index.ts'),
+        'utils-api': path.resolve(__dirname, 'src/utils-api/index.ts'),
+        'utils-event': path.resolve(__dirname, 'src/utils-event/index.ts'),
+        'utils-lang': path.resolve(__dirname, 'src/utils-lang/index.ts'),
+        'utils-log': path.resolve(__dirname, 'src/utils-log/index.ts'),
+        'utils-tests': path.resolve(__dirname, 'src/utils-tests/index.ts'),
+        'utils-types': path.resolve(__dirname, 'src/utils-types/index.ts'),
+        'utils-web-storage': path.resolve(
+          __dirname,
+          'src/utils-web-storage/index.ts',
+        ),
+      },
+      formats: ['es'],
+      fileName: (_, entryName) => `${entryName}/index.js`,
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: [],
+    },
+  },
+  plugins: [
+    dts({
+      entryRoot: 'src',
+      outDir: 'dist',
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.test.ts'],
+  },
+});
